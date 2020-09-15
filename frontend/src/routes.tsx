5062f660b4270e92d4e79141ec652f65fd012d8f
ElijahLopp <eliaslopes657@gmail.com>
2020-09-15 14:49:35 -0300

    Fisrt commit

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Import any page here
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      {/* <Route path="/" exact component={Page here}/> */}
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
